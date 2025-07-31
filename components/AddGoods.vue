6e47c270761196912e0a2e8b8f84de6dcbbba1f6
hir01 <glazunovden@yandex.ru>
2025-07-31 13:34:15 +0300

    верстка

--- components/AddGoods.vue
+++ components/AddGoods.vue
@@ -1,9 +1,32 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type { FileUploadUploaderEvent } from "primevue";
+
+const toast = useToast();
+
+const upLoader = async (event: FileUploadUploaderEvent) => {
+  let file: File | undefined;
+
+  if (Array.isArray(event.files)) {
+    file = event.files[0];
+  } else {
+    file = event.files;
+  }
+
+  if (!file) return;
+
+  toast.add({
+    severity: "success",
+    summary: "Файл загружен",
+    detail: `Файл ${file.name} успешно загружен.`,
+    life: 3000,
+  });
+};
+</script>
 
 <template>
   <section>
     <div class="container m-auto">
-      <div class="grid grid-cols-8">
+      <div class="grid grid-cols-8 gap-20">
         <div class="col-span-6">
           <h1 class="mb-40">Загрузить товары через шаблон</h1>
           <div class="flex flex-col gap-40">
@@ -28,7 +51,56 @@
             </div>
             <div class="col">
               <h3 class="mb-12">Шаг 3. Загрузите шаблон</h3>
-              <Button size="large" class="!rounded-2xl">Скачать шаблон</Button>
+              <div class="w-320">
+                <!-- 10485760 -->
+                <FileUpload
+                  :custom-upload="true"
+                  :auto="true"
+                  name="file"
+                  accept=".xls, .xlsx, .xlsm"
+                  :max-file-size="10485760"
+                  invalid-file-size-message="Максимальный размер файла 10 МБ"
+                  choose-label="Загрузить файл"
+                  unstyled
+                  :pt="{
+                    input: {
+                      class: 'hidden',
+                    },
+                  }"
+                  @uploader="upLoader"
+                >
+                  <template #header="{ chooseCallback }">
+                    <Button
+                      class="flex flex-col !rounded-2xl w-full"
+                      size="large"
+                      @click="chooseCallback()"
+                    >
+                      Выберите файл
+                      <span class="text-sm opacity-50">
+                        Формат - .XLS .XLSX .XLSM. Не больше 10 МБ
+                      </span>
+                    </Button>
+                  </template>
+                  <template #content="{ files, uploadedFiles, messages }">
+
+                    <div class="flex flex-col gap-8">
+                      <Message
+                        v-for="message of messages"
+                        :key="message"
+                        class="mt-8 items-center"
+                        severity="error"
+                        :pt="{
+                          content: { class: 'justify-center' },
+                        }"
+                      >
+                        {{ message }}
+                      </Message>
+                      <div v-if="files.length > 0"></div>
+                      <div v-if="uploadedFiles.length > 0"></div>
+                    </div>
+                  </template>
+                </FileUpload>
+              </div>
             </div>
             <div class="col">
               <Button severity="contrast" size="large" class="!rounded-2xl">
@@ -37,7 +109,18 @@
             </div>
           </div>
         </div>
-        <div class="col-span-2">Если нужна помощь</div>
+        <div class="col-span-2">
+          <div class="p-30 rounded-2xl bg-gray-100">
+            <h3 class="mb-12">Если нужна помощь</h3>
+            <p>
+              Вы можете задать вопрос в чате нашему менеджеру прямо по этой
+              кнопке
+            </p>
+            <Button size="large" class="!rounded-2xl mt-20">
+              Помощь менеджера
+            </Button>
+          </div>
+        </div>
       </div>
     </div>
   </section>
